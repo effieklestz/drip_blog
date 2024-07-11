3adb4d53d5e40e4cc433a121369ca5598e5bbd5f
ephraimklestzick <effieklestz@yahoo.com>
2024-07-11 16:52:02 -0400

    migraions

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from datetime import datetime
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Categories(models.TextChoices):
+  WORLD = 'world'
+  TRAVEL = 'travel'
+  COFFEE = 'coffee'
+  WORK_LIFE = 'work_life'
+  DIGITAL_NOMAD = 'digital_nomad'
+  FOOD = 'food'
+  HIKES = 'hikes'
+  BEACHES = 'beaches'
+
+
+class BlogPost(models.Model):
+  title =  models.CharField(max_length=50)
+  slug = models.SlugField()
+  category = models.CharField(max_length=50, choices=Categories.choices, default=Categories.WORLD)
+  thumbnail = models.ImageField(upload_to='photo/%Y/%m/%d/')
+  excerpt = models.CharField(max_length=200)
+  month = models.CharField(max_length=3)
+  day = models.CharField(max_length=2)
+  content = models.TextField()
+  featured = models.BooleanField(default=False)
+  date_created = models.DateTimeField(default=datetime.now, blank=True)
+
+
+  def save(self, *args, **kwargs):
+    original_slug =slugify(self.title)
+    queryset = BlogPost.objects.all().filter(slug_iexact=slug).count()
+
+    count = 1
+    slug = original_slug
+    while(queryset):
+      slug = original_slug + '-' + str(count)
+      count += 1
+      queryset = BlogPost.objects.all().filter(slug_iexact=original_slug).count()
+
+    self.slug = slug
+    if self.featured:
+      try:
+        temp = BlogPost.objects.get(featured= True)
+        if self != temp:
+          temp.featured =False
+          temp.save()
+
+      except  BlogPost.DoesNotExist:
+        pass
+    super(BlogPost, self).save(*args,**kwargs)
+  
+  def __str__(self):
+    return self.title
--- backend/drip_blog/settings.py
+++ backend/drip_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,10 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
-    'corsheaders'
-    'djangosummernote'
-    'blog'
+    'rest_framework',
+    'corsheaders',
+    'django_summernote',
+    'blog',
 
 ]
 
@@ -125,12 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS =[
   os.path.join(BASE_DIR, 'build/static')
   ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static'
-                           )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
@@ -141,5 +141,9 @@ REST_FRAMEWORK = {
 }
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+SUMMERNOTE_THEME = 'bs4'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
